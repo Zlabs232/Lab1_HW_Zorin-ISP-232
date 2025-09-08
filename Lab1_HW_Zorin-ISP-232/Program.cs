a0d5a39afe3b5e0d76739bb208d59bf9477e6b27
Роман Зорин <sgena170315@gmail.com>
2025-09-08 11:10:38 +0300

    Задание 2. Мини-анкетa

--- Lab1_HW_Zorin-ISP-232/Program.cs
+++ Lab1_HW_Zorin-ISP-232/Program.cs
@@ -15,6 +15,19 @@
             Console.WriteLine($"{x} * {y} = {x * y}");
             double res = x / y;
             Console.WriteLine($"{x} * {y} = {res}");
+
+            //Task 2
+
+            Console.WriteLine("Введите имя пользователя: ");
+            string? name = Console.ReadLine();
+            Console.WriteLine("Введите возраст пользователя: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите любимый язык программирования: ");
+            string? PL = Console.ReadLine();
+
+            Console.WriteLine($"Привет, {name}!\nТебе {age} лет.\nТвой любимый язык программирования: {PL}.");
+
+
         }
     }
 }
