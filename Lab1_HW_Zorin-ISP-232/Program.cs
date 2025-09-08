703c8dd97f685e83a2f4a06d8f696ed2140f5b37
Роман Зорин <sgena170315@gmail.com>
2025-09-08 11:48:02 +0300

    Задание 4: "Персональный профиль пользователя"

--- Lab1_HW_Zorin-ISP-232/Program.cs
+++ Lab1_HW_Zorin-ISP-232/Program.cs
@@ -35,6 +35,27 @@
 
             Console.WriteLine($"Выражение f(x) = ax^2 + bx + c с подставленными значениями = {a * Math.Pow(5,2) + b * 5 + c}");
 
+            //Task 4
+            Console.Write("Введите имя: ");
+            string? name1 = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            int age1 = int.Parse(Console.ReadLine());
+            Console.Write("Введите город: ");
+            string? city = Console.ReadLine();
+            Console.Write("Введите любимое число: ");
+            int FN = int.Parse(Console.ReadLine());
+            Console.Write("Введите температуру в градусах Цельсия° за окном: ");
+            int cel = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" ");
+            Console.WriteLine("=== ПЕРСОНАЛЬНЫЙ ПРОФИЛЬ ===");
+            Console.WriteLine(" ");
+
+            Console.WriteLine($"Привет, {name1}!\nТебе {age1} лет.\nВ следующем году тебе будет {age1+1}.\n\nТы живешь в городе {city}.\nТвоё любимое число: {FN} (в квадрате: {Math.Pow(FN, 2)})\n\nТемпература за окном:\n- В цельсиях: {cel}°C\n- В Фаренгейтах: {(cel * 9 / 5) + 32}\n");
+
+            Console.WriteLine("=== КОНЕЦ ПРОФИЛЯ ===");
+
+
 
         }
     }
