94d6f6899f9c8fd0d7be6d1c7e413ba5c7d640e9
Роман Зорин <sgena170315@gmail.com>
2025-09-08 11:06:36 +0300

    Add project files.

---
+++ Lab1_HW_Zorin-ISP-232/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab1_HW_Zorin_ISP_232
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            Console.WriteLine("Введите первое число: ");
+            int x = int.Parse(Console.ReadLine()); //a = int(input()) 
+            Console.WriteLine("Введите второе число: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{x} + {y} = {x + y}");
+            Console.WriteLine($"{x} - {y} = {x - y}");
+            Console.WriteLine($"{x} * {y} = {x * y}");
+            double res = x / y;
+            Console.WriteLine($"{x} * {y} = {res}");
+        }
+    }
+}
